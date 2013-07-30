36dd4ad9fe36785920969144ac2bd5de23d2404c
Aurynn Shaw <self@aurynn.com>
2013-07-31 11:07:25 +1200

    Initial template-ness

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,12 @@
 # Create your views here.
-
+from django.template import Context, loader
 from django.http import HttpResponse
 from todo.models import Item
 
 def index(self):
 
 	# return HttpResponse("At the beginning..")
-	return HttpResponse(Item.objects.all())
+	template = loader.get_template("index.htmlt")
+	c = Context({"todo": Item.objects.all()})
+
+	return HttpResponse(template.render(c))
