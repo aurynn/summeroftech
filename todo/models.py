aa33fd66b9bb64b000d9e0327f00a66bfffb6f91
Aurynn Shaw <self@aurynn.com>
2013-07-30 20:42:29 +1200

    Basic model created

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+
+	# id is auto-generated
+	content = models.CharField(max_length=200)
+	created = models.DateTimeField(auto_now_add=True)
+	done = models.BooleanField()
