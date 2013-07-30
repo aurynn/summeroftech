5a5163839aaaa22856b749e7b8e0c80d4247afd5
Aurynn Shaw <self@aurynn.com>
2013-07-30 20:57:14 +1200

    Item class works as expected

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Item(models.Model):
 
 	# id is auto-generated
 	content = models.CharField(max_length=200)
-	created = models.DateTimeField(auto_now_add=True)
+	created = models.DateTimeField()
 	done = models.BooleanField()
+
+	def __init__(self, content):
+		self.content = content
+		self.created = datetime.datetime.now()
+		self.done = False
