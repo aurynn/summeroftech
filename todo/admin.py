0ac36c03dcb624d9de42ae42cf9e3faf9d1b577f
Aurynn Shaw <self@aurynn.com>
2013-07-30 21:12:25 +1200

    Item works as expected in the Admin interface. We're done.

---
+++ todo/admin.py
@@ -0,0 +1,4 @@
+from todo.models import Item
+from django.contrib import admin
+
+admin.site.register(Item)
--- todo/models.py
+++ todo/models.py
@@ -4,12 +4,10 @@ import datetime
 # Create your models here.
 class Item(models.Model):
 
-	# id is auto-generated
+	# id	    = models.
 	content = models.CharField(max_length=200)
 	created = models.DateTimeField()
 	done = models.BooleanField()
 
-	def __init__(self, content):
-		self.content = content
-		self.created = datetime.datetime.now()
-		self.done = False
+	def __str__(self):
+		return self.content
