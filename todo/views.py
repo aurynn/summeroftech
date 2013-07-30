cb8587051e1004259c35aaccb1a793f50416a1c4
Aurynn Shaw <self@aurynn.com>
2013-07-31 10:38:46 +1200

    Simple rendering of things stored in our todo

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from todo.models import Item
 
 def index(self):
 
-	return HttpResponse("At the beginning..")
+	# return HttpResponse("At the beginning..")
+	return HttpResponse(Item.objects.all())
